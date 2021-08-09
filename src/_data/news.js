609e1ff90f0afbf594f55198ec6804841553dbcf
Craigc <craigclement@hotmail.com>
2021-08-09 21:40:13 +0100

    end of day

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,9 +1,11 @@
 // const axios = require("axios");
 const axios = require('axios');
 
+const API_KEY = 'f31920bab9ae436bab2b02e8317fad5a'
+
 module.exports = async function () {
   try {
-    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=gb&apiKey=f31920bab9ae436bab2b02e8317fad5a&pageSize=5')
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${API_KEY}&pageSize=5`)
     console.log(response.data)
     return (response.data);
   } catch (err) {
