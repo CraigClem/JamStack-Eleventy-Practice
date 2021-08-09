3616983ff9904d62b8adadf9de3223dded85685d
Craigc <craigclement@hotmail.com>
2021-08-09 19:19:05 +0100

    array of objects

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function (config) {
 
+  //? addPassthroughCopy is a method that takes a folder file as an argument so that eleventy can run JavaScript as it does not read .js files to add to the dist folder. 
+
   config.addPassthroughCopy('src/js');
 
   return {
     dir: {
       input: 'src',
-      output: 'dist'
+      output: 'dist',
+      data: '_data'
     }
   };
 }
