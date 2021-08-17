7529210dec8f7357036bba1b4c39ec97d72ecbb0
Craig <craigclement@hotmail.com>
2021-08-17 10:33:03 +0100

    news

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,28 +1,25 @@
 
 const axios = require('axios');
-const countries = require('./countries.json');
+// const countries = require('./countries.json');
 
 require('dotenv').config();
 
-async function getNews(country) {
+module.exports = async function () {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`)
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}&pageSize=5`)
     console.log(response.data)
-    return {
-      "country": country,
-      "article": response.data
-    }
+    return (response.data)
   } catch (err) {
     console.log(err)
   }
 }
 
-module.exports = async function () {
+// module.exports = async function () {
 
-  var newsPromises = countries.map(getNews);
-  return Promise.all(newsPromises).then(newsObjects => {
-    console.log('newsObjects:', newsObjects);
-    return [].concat.apply([], newsObjects);
-  });
-}
+//   var newsPromises = countries.map(getNews);
+//   return Promise.all(newsPromises).then(newsObjects => {
+//     console.log('newsObjects:', newsObjects);
+//     return [].concat.apply([], newsObjects);
+//   });
+// }
 
