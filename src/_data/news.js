484a838e5c391ac6b9cb3adb38a2424590bbf057
Craig <craigclement@hotmail.com>
2021-08-17 10:15:48 +0100

    news object per country code

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,28 @@
-// const axios = require("axios");
+
 const axios = require('axios');
+const countries = require('./countries.json');
+
 require('dotenv').config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}&pageSize=5`)
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`)
     console.log(response.data)
-    return (response.data);
+    return {
+      "country": country,
+      "article": response.data
+    }
   } catch (err) {
     console.log(err)
   }
 }
+
+module.exports = async function () {
+
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then(newsObjects => {
+    console.log('newsObjects:', newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
+}
+
