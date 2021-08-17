0f985778c582f33abda4db728744b79ebf6f4b51
Craig <craigclement@hotmail.com>
2021-08-17 12:21:58 +0100

    map over countries

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,25 +1,44 @@
 
 const axios = require('axios');
-// const countries = require('./countries.json');
+const countries = require('./countries.json');
+console.log(countries)
 
 require('dotenv').config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}&pageSize=5`)
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`)
     console.log(response.data)
-    return (response.data)
+    return {
+      "country": country,
+      "articles": response.data.articles
+    }
   } catch (err) {
     console.log(err)
   }
+};
+
+
+
+module.exports = async function () {
+
+  const newsPromises = countries.map(getNews)
+  return Promise.all(newsPromises).then(newsObjects => {
+    console.log('newsObjects:', newsObjects)
+    return [].concat.apply([], newsObjects)
+  })
+
 }
 
-// module.exports = async function () {
 
-//   var newsPromises = countries.map(getNews);
-//   return Promise.all(newsPromises).then(newsObjects => {
-//     console.log('newsObjects:', newsObjects);
-//     return [].concat.apply([], newsObjects);
-//   });
-// }
+// module.exports = async function () {
+//   try {
+//     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}&pageSize=5`)
+//     console.log(response.data)
+//     return response.data;
+//   } catch (err) {
+//     console.log(err)
+//   }
+// };
+
 
